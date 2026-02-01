aade5baffe2597590c1b3b0ca81662788bde2133
Naoki Inoue <inoue.naoki.job@gmail.com>
2026-02-01 18:45:09 +0900

    feat: Vite + React プロジェクトの初期化とディレクトリ構成の整理

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="app-container">
+      <header>
+        <h1>Synthesizer Learning</h1>
+      </header>
+      <main>
+        <p>Welcome to the Synthesizer Learning Platform.</p>
+        <div className="synth-placeholder">
+          {/* Synth components will go here */}
+          <p>Synthesizer UI Loading...</p>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
